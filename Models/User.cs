b0c936fe1b52132a6e4f4e9fe71b7f06a1169651
George Bjork <georgebjork@outlook.com>
2021-04-20 20:28:34 -0700

    can now reciver user login and password

--- Models/User.cs
+++ Models/User.cs
@@ -12,6 +12,8 @@ namespace api_whitTasks.Models
         public string first_name { get; set; }
         public string last_name { get; set; }
         public DateTime date {get; set; }
+        public string email {get; set;}
+        public string password{get; set;}
 
         public User(){}
 
@@ -21,6 +23,8 @@ namespace api_whitTasks.Models
             this.first_name = (string)dr["first_name"];
             this.last_name = (string)dr["last_name"];
             this.date = (DateTime)dr["created_date"];
+            this.email = (string)dr["email"];
+            this.password = (string)dr["password"];
         }
 
 
